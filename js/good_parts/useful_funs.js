71e3bce6efef95b9484afa5e487aa0a20a2248ed
Colin Ji <jichen3000@gmail.com>
2013-03-21 17:32:37 +0800

    add some

--- js/good_parts/useful_funs.js
+++ js/good_parts/useful_funs.js
@@ -12,3 +12,17 @@ String.set_method('trim', function () {
 });
 
 document.writeln('"' + " neat ".trim() + '"');
+
+// Make a factorial function with tail
+// recursion. It is tail recursive because
+// it returns the result of calling itself.
+// JavaScript does not currently optimize this form.
+
+var factorial = function factorial (i, a) {
+    a = a || 1;
+    if (i < 2) {
+        return a;
+    };
+    return factorial(i - 1, a * i);
+}
+
--- python/my_test/change_self.py
+++ python/my_test/change_self.py
@@ -3,12 +3,51 @@ import re
 from datetime import datetime
 
 __MAJOR = 1
-__MINOR = 2
-__BUILD = 3
-__BUILT_TIME_STR = "2013-03-20 16:47:50"
-__BUILT_TIME = datetime.strptime(__BUILT_TIME_STR,'%Y-%m-%d %H:%M:%S')
-__BUILD_PATTERN = re.compile('^(__BUILD)\s*=\s*(\d+)')
-__BUILT_TIME_PATTERN = re.compile(''^(__BUILT_TIME_STR)\s*=\s*"(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2})"'')
+__MINOR = 1
+__BUILD = 1
+__BUILT_TIME_STR = "2013-03-21 17:27:33"
+__BUILT_TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+__BUILD_NAME = "__BUILD"
+__BUILT_TIME_STR_NAME = "__BUILT_TIME_STR"
+__BUILT_TIME = datetime.strptime(__BUILT_TIME_STR, __BUILT_TIME_FORMAT)
+__BUILD_PATTERN = re.compile('^('+__BUILD_NAME+')\s*=\s*(\d+)')
+__BUILT_TIME_PATTERN = re.compile('^('+__BUILT_TIME_STR_NAME+')\s*=\s*"(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2})"')
+__PATTERN_ARRAY = (__BUILD_PATTERN, __BUILT_TIME_PATTERN)
+
+def __read_lines(file_name):
+    with open(file_name, 'r') as f:
+        return f.readlines()
+
+def __write_lines(file_name, lines):
+    with open(file_name, 'w') as f:
+        f.writelines(lines)
+    return True
+
+def __get_need_change(line):
+    for pattern in __PATTERN_ARRAY:
+        if pattern.match(line) != None:
+            return pattern.match(line)
+    return None
+
+def __change_values(line):
+    name, value = __get_need_change(line).groups()
+    function_name_suffix = name.lower().replace('__','_')
+    fun = eval("__change" + function_name_suffix)
+    return fun(line, value)
+
+def __change_built_time_str(line, value):
+    return line.replace(value,datetime.now().strftime(__BUILT_TIME_FORMAT))
+    
+
+def __change_build(line, value):
+    pre_build_no = int(value)
+    return line.replace(str(pre_build_no),str(pre_build_no+1))
+
+def __get_info_values():
+    match_list = [__get_need_change(line) for line in __read_lines(os.path.realpath(__file__))]
+    match_list = filter (lambda a: a != None, match_list)
+    match_list = {i.groups()[0]:i.groups()[1] for i in match_list}
+    return  match_list
 
 def get_major():
     return __MAJOR
@@ -22,34 +61,33 @@ def get_build():
 def get_built_time():
     return __BUILT_TIME
 
-def __read_lines(file_name):
-    with open(file_name, 'r') as f:
-        return f.readlines()
+def __get_version_by_build_str(build_str):
+    return str(get_major())+"."+str(get_minor())+"."+build_str
 
-def __write_lines(file_name, lines):
-    with open(file_name, 'w') as f:
-        return f.writelines(lines)
+def get_version():
+    return __get_version_by_build_str(str(get_build()))
 
-def is_need_change(line):
-    return __BUILD_PATTERN.match(line) != None
-
-def change_values(line):
-    name, value = __BUILD_PATTERN.match(line).groups()
-
-def change_built_time(line, value):
-    now = datetime.now().strftime('%Y%m%d%H%M%S')
-
-def change_version(line, value):
-    pre_build_no = int(value)
-    return line.replace(str(pre_build_no),str(pre_build_no+1))
-
-
-def gen_version():
+def get_info():
+    info_values = __get_info_values()
+    return "The current version is "+__get_version_by_build_str(
+        info_values[__BUILD_NAME])+", and built time is "+info_values[__BUILT_TIME_STR_NAME]+"."
+    
+def new_version():
     self_file_name = os.path.realpath(__file__)
     lines = __read_lines(self_file_name)
-    new_lines = [change_version(line) if is_need_change(line) else line for line in lines]
-    print new_lines
-    # __write_lines(self_file_name, lines)
+    new_lines = [__change_values(line) if __get_need_change(line) != None else line for line in lines]
+    __write_lines(self_file_name, new_lines)
+    return True
 
 if __name__ == '__main__':
-    gen_version()
+    import sys
+    if (len(sys.argv) == 2) and (sys.argv[1]=="new_version"):
+        new_version()
+        print "A new version has been generated!"
+        print get_info()
+    elif (len(sys.argv) == 2) and (sys.argv[1]=="get_info"):
+        print get_info()
+    else:
+        print "Please use the below commands to generate a new version."
+        print "  python gen_version.py new_version"
+        print "  python gen_version.py get_info"
