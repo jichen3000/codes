e4fa4954ab0ddede90a7db2a31199832ab9b8b62
Colin Ji <jichen3000@gmail.com>
2013-03-26 17:48:13 +0800

    add some

--- patterns/single_var_pattern.js
+++
@@ -1,8 +0,0 @@
-// antipattern
-myname = "global"; // global variable 
-function func() {
-    console.log(myname); // "undefined" 
-    var myname = "local"; 
-    console.log(myname); // "local"
-} 
-func();
