85c05ecc988beda5891d5836b2af5a3452e7f52d
Colin Ji <jichen3000@gmail.com>
2013-06-11 22:48:41 +0800

    add some

--- python/my_test/bottle_simple.py
+++ python/my_test/bottle_simple.py
@@ -1,7 +1,10 @@
 from bottle import route, run, template
+import BaseHTTPServer
 
 @route('/hello/:name')
 def index(name='World'):
 	return template('<b>Hello {{name}} </b>', name=name)
 
+setattr(BaseHTTPServer.HTTPServer,'allow_reuse_address',0)
+# run(host='localhost', port=9999, allow_reuse_address=0)
 run(host='localhost', port=9999)
